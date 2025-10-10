5b8416527b8b8201f336e6fb7d75184d3fab1e9f
Pooja <poozagrg432@gmail.com>
2025-10-10 21:16:19 +0545

    home page

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,32 @@
 import { images, offers } from "@/constants";
 import { Fragment } from "react";
-import { FlatList, Image, Pressable, Text, View } from "react-native";
+import { FlatList, Image, Pressable, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import cn from 'clsx';
  
 export default function Index() {
   return (
     <SafeAreaView className="flex-1 bg-white">
+      <ScrollView>
+      <View className="flex-between flex-row w-full my-5 px-5">
+        <View className="flex-start">
+          <Text className="small-bold text-primary">Deliver To</Text>
+          <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
+            <Text className="paragraph-bold text-dark-100">Nepal</Text>
+            <Image source={images.arrowDown} className="size-3" resizeMode="contain"/>
+          </TouchableOpacity>
+        </View>
+        <Text>Cart</Text>
+      </View>
       <FlatList 
         data={offers} 
         renderItem={({ item, index }) => {
           const isEven: boolean = index % 2 == 0;
           return (
             <View>
-              <Pressable className={cn("offer-card", isEven ? 'flex-row-reverse' : 'flex-row')} style={{backgroundColor: item.color}}>
+              <Pressable className={cn("offer-card", isEven ? 'flex-row-reverse' : 'flex-row')} style={{backgroundColor: item.color}}
+              android_ripple={{ color: "#ffff22" }}
+              >
                 {({ pressed }) => (
                   <Fragment>
                     <View className="h-full w-1/2">
@@ -21,7 +34,7 @@ export default function Index() {
                     </View>
                   <View className = {cn(
                         "offer-card__info",
-                        isEven ? "items-end" : "items-start",
+                        isEven ? "pl-10" : "pr-10",
                         "flex-1 justify-center px-4"
                       )}>
                       <Text className="h1-bold text-white leading-tight">
@@ -40,8 +53,9 @@ export default function Index() {
             </View>
           )
         }}
-        
+        contentContainerClassName="pb-28 px-5"
       />
+      </ScrollView>
     </SafeAreaView>
   );
 }
